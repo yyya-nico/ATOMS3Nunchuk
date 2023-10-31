2cf4637791c412e2769db1dc449a2cbea7d1129a
yyya_nico <hirano4650026@gmail.com>
2023-10-31 21:41:22 +0900

    LEDをインジケーターに、スティックの不感域をポインター操作にも

--- ATOMS3Nunchuk/ATOMS3Nunchuk.ino
+++ ATOMS3Nunchuk/ATOMS3Nunchuk.ino
@@ -1,13 +1,15 @@
+#include <M5AtomS3.h>
 #include "Wire.h"
 #include <BleMouse.h>
 
 BleMouse bleMouse("Nunchuck Mouse", "Created by yyya_nico");
 
+#define INITIAL_COLOR 0x002600
 #define NUNCHK_ADDR (0x52)
 #define COMPLETE_COUNT (5)
 #define NUNCHK_Z_MASK 0x01
 #define NUNCHK_C_MASK 0x02
-#define POSITION_MARGIN 10
+#define POSITION_MARGIN 5
 
 static int initXposi = 0;
 static int initYposi = 0;
@@ -18,66 +20,78 @@ static bool disableScroll = false;
 void setup() {
   bleMouse.begin();
   initNunchuk();
+  M5.begin(false, true, false, true);
+  M5.dis.drawpix(INITIAL_COLOR);
+  M5.dis.show();
 }
 
 void loop() {
   int     x      = 0;
   int     y      = 0;
   uint8_t button = 0;
-  if(bleMouse.isConnected()  && nunchuckIsAvailable(&x, &y, &button) ){
-    int xPosi = x - initXposi;
-    int yPosi = y - initYposi;
+  if(bleMouse.isConnected()){
+    M5.dis.drawpix(0x000026);
+    M5.dis.show();
+    if(nunchuckIsAvailable(&x, &y, &button) ){
+      int xPosi = x - initXposi;
+      int yPosi = y - initYposi;
 
-    if(button & NUNCHK_C_MASK) { // c button
-      if(intervalCount >= 1000/10) {
-        bleMouse.press(MOUSE_RIGHT);
-        intervalCount = 0;
-        disableCount = true;
-        disableScroll = true;
-        delay(10);
-        bleMouse.release(MOUSE_RIGHT);
+      if(button & NUNCHK_C_MASK) { // c button
+        if(intervalCount >= 1000/10) {
+          bleMouse.press(MOUSE_RIGHT);
+          intervalCount = 0;
+          disableCount = true;
+          disableScroll = true;
+          delay(10);
+          bleMouse.release(MOUSE_RIGHT);
+        }
+        else {
+          if(abs(xPosi) > POSITION_MARGIN || abs(yPosi) > POSITION_MARGIN) {
+            intervalCount = 0;
+            disableCount = true;
+            if(yPosi > POSITION_MARGIN && !disableScroll) {
+              bleMouse.move(0, 0, 1);
+            }
+            else if(yPosi < -POSITION_MARGIN && !disableScroll) {
+              bleMouse.move(0, 0, -1);
+            }
+            int delay1 = max(-3 * abs(yPosi) + 240, 0);
+            if(xPosi > POSITION_MARGIN && !disableScroll) {
+              bleMouse.move(0, 0, 0, 1);
+            }
+            else if(xPosi < -POSITION_MARGIN && !disableScroll) {
+              bleMouse.move(0, 0, 0, -1);
+            }
+            int delay2 = max(-3 * abs(xPosi) + 240, 0);
+            delay(min(delay1, delay2));
+          }
+          else if(!disableCount) {
+            intervalCount++;
+          }
+        }
       }
       else {
         if(abs(xPosi) > POSITION_MARGIN || abs(yPosi) > POSITION_MARGIN) {
-          intervalCount = 0;
-          disableCount = true;
-          if(yPosi > POSITION_MARGIN && !disableScroll) {
-            bleMouse.move(0, 0, 1);
-          }
-          else if(yPosi < -POSITION_MARGIN && !disableScroll) {
-            bleMouse.move(0, 0, -1);
-          }
-          int delay1 = max(-3 * abs(yPosi) + 240, 0);
-          if(xPosi > POSITION_MARGIN && !disableScroll) {
-            bleMouse.move(0, 0, 0, 1);
-          }
-          else if(xPosi < -POSITION_MARGIN && !disableScroll) {
-            bleMouse.move(0, 0, 0, -1);
-          }
-          int delay2 = max(-3 * abs(xPosi) + 240, 0);
-          delay(min(delay1, delay2));
+          bleMouse.move((signed char)(xPosi)*50/127, -(signed char)(yPosi)*50/127); // stick position x y
         }
-        else if(!disableCount) {
-          intervalCount++;
-        }
+        intervalCount = 0;
+        disableCount = false;
+        disableScroll = false;
+      }
+
+      if(button & NUNCHK_Z_MASK) { // z button 
+        bleMouse.press(MOUSE_LEFT);
+      }
+      else {
+        bleMouse.release(MOUSE_LEFT);
       }
     }
     else {
-      bleMouse.move((signed char)(xPosi)*50/127, -(signed char)(yPosi)*50/127); // stick position x y
-      intervalCount = 0;
-      disableCount = false;
-      disableScroll = false;
+      bleMouse.release(MOUSE_ALL);
     }
-
-    if(button & NUNCHK_Z_MASK) { // z button 
-      bleMouse.press(MOUSE_LEFT);
-    }
-    else {
-      bleMouse.release(MOUSE_LEFT);
-    }
-  }
-  else {
-    bleMouse.release(MOUSE_ALL);
+  } else {
+    M5.dis.drawpix(INITIAL_COLOR);
+    M5.dis.show();
   }
   delay(10);
 }
